f54ac723b7b15c5edf088d4460e5c23d820a5cb1
Juan Carlos Hernandez Ramos <jc.hernandez129@uniandes.edu.co>
2019-02-15 00:39:17 +0100

    UseKinect Gestures

---
+++ DIM-Kinect/KinectChooser.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DIM_Kinect
+{
+    /// <summary>
+    /// Interaction logic for KineckChooser.xaml
+    /// </summary>
+    public partial class KinectChooser : Page
+    {
+        private KinectSensorChooser sensorChooser;
+        public KinectChooser()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.sensorChooser = new KinectSensorChooser(); this.sensorChooser.KinectChanged += SensorChooserOnKinectChanged; this.sensorChooserUi.KinectSensorChooser = this.sensorChooser; this.sensorChooser.Start();
+        }
+
+        private void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs args)
+        {
+            bool error = false;
+            if (args.OldSensor != null)
+            {
+                try
+                {
+                    args.OldSensor.DepthStream.Range = DepthRange.Default;
+                    args.OldSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                    args.OldSensor.DepthStream.Disable();
+                    args.OldSensor.SkeletonStream.Disable();
+                }
+                catch (InvalidOperationException)
+                {
+                    // KinectSensor entra en estado invalid mientras se habilitan/deshabilitan  
+                    // streams.  
+                    // Ej.: Se desconecta sensor de forma abrupta.   
+                    error = true;
+                }
+            }
+            if (args.NewSensor != null)
+            {
+                try
+                {
+                    args.NewSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+                    args.NewSensor.SkeletonStream.Enable();
+                }
+                catch (InvalidOperationException)
+                {
+                    error = true;
+                }
+            }
+            if (!error) kinectRegion.KinectSensor = args.NewSensor;
+        }
+
+        private void ButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("¡Estupendo!");
+        }
+    }
+}
---
+++ DIM-Kinect/KinectInteraction.xaml.cs
@@ -0,0 +1,298 @@
+﻿using Kinect.Toolbox;
+using Microsoft.Kinect;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DIM_Kinect
+{
+    /// <summary>
+    /// Interaction logic for KinectInteraction.xaml
+    /// </summary>
+    public partial class KinectInteraction : Page
+    {
+        private KinectSensor sensor;
+        private SwipeGestureDetector swipeGestureRecognizer;
+        private TemplatedGestureDetector circleGestureRecognizer;
+
+        public KinectInteraction()
+        {
+            InitializeComponent();
+            Loaded += Window_Loaded;
+        }
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (KinectSensor potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            try
+            {
+                this.sensor.ColorFrameReady += SensorColorFrameReady;
+                this.sensor.DepthFrameReady += SensorDepthFrameReady;
+                this.sensor.SkeletonFrameReady += miKinect_SkeletonFrameReady;
+                swipeGestureRecognizer = new SwipeGestureDetector();
+                swipeGestureRecognizer.OnGestureDetected += OnGestureDetectedSwipe;
+
+                using (Stream recordStream = File.Open(System.IO.Path.Combine(Environment.CurrentDirectory, @"Templates\circleKB.save"), FileMode.Open))
+                {
+                    circleGestureRecognizer = new TemplatedGestureDetector("Circle", recordStream);
+                    circleGestureRecognizer.OnGestureDetected += OnGestureDetectedCircle;
+                }
+            }
+            catch
+            {
+                invalidate();
+                msj_Label.Content = "Kinect can not be initialized.";
+            }
+        }
+
+        private void OnGestureDetectedCircle(string gesture)
+        {
+            switch (gesture)
+            {
+                case "Circle":
+                    this.canvasEsqueleto.Visibility = this.canvasEsqueleto.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void OnGestureDetectedSwipe(string gesture)
+        {
+            switch (gesture)
+            {
+                case "SwipeToLeft":
+                    if (this.ColorSelection.IsChecked == true) this.IRSelection.IsChecked = true;
+                    else if (this.IRSelection.IsChecked == true) this.ColorSelection.IsChecked = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void invalidate()
+        {
+            if (this.sensor != null)
+            {
+                this.sensor.ColorFrameReady -= SensorColorFrameReady;
+                this.sensor.DepthFrameReady -= SensorDepthFrameReady;
+                this.sensor.SkeletonFrameReady -= miKinect_SkeletonFrameReady;
+                this.sensor.Stop();
+                this.sensor = null;
+            }
+        }
+
+        private void SensorDepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            if (this.sensor != null)
+            {
+                DepthImagePixel[] depthPixels = new DepthImagePixel[this.sensor.DepthStream.FramePixelDataLength];
+
+                byte[] colorPixels = new byte[this.sensor.DepthStream.FramePixelDataLength * sizeof(int)];
+                WriteableBitmap colorBitmap = new WriteableBitmap(this.sensor.DepthStream.FrameWidth, this.sensor.DepthStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+                {
+                    if (depthFrame != null)
+                    {
+                        depthFrame.CopyDepthImagePixelDataTo(depthPixels);
+
+
+                        int minDepth = depthFrame.MinDepth;
+                        int maxDepth = depthFrame.MaxDepth;
+
+                        int colorPixelIndex = 0;
+                        for (int i = 0; i < depthPixels.Length; i++)
+                        {
+                            short depth = depthPixels[i].Depth;
+                            byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+                            colorPixels[colorPixelIndex++] = intensity;
+                            colorPixels[colorPixelIndex++] = intensity;
+                            colorPixels[colorPixelIndex++] = intensity;
+                            colorPixelIndex++; // no alpha channel RGB
+                        }
+
+                        colorBitmap.WritePixels(new Int32Rect(0, 0, colorBitmap.PixelWidth, colorBitmap.PixelHeight), colorPixels, colorBitmap.PixelWidth * sizeof(int), 0);
+
+                        this.kinect_Img.Source = colorBitmap;
+                    }
+                }
+            }
+        }
+
+        private void SensorColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            if (this.sensor != null)
+            {
+                byte[] colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+                WriteableBitmap colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth,
+                this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+                {
+                    if (colorFrame != null)
+                    {
+                        colorFrame.CopyPixelDataTo(colorPixels);
+                        colorBitmap.WritePixels(new Int32Rect(0, 0, colorBitmap.PixelWidth, colorBitmap.PixelHeight),
+                        colorPixels, colorBitmap.PixelWidth * sizeof(int), 0);
+                        this.kinect_Img.Source = colorBitmap;
+                    }
+                }
+            }
+        }
+
+        private void miKinect_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            canvasEsqueleto.Children.Clear();
+            Skeleton[] Skeleton = null;
+
+            using (SkeletonFrame frSkeleton = e.OpenSkeletonFrame())
+            {
+                if (frSkeleton != null)
+                {
+                    Skeleton = new Skeleton[frSkeleton.SkeletonArrayLength];
+                    frSkeleton.CopySkeletonDataTo(Skeleton);
+                }
+            }
+
+            if (Skeleton == null) return;
+
+            foreach (Skeleton skeleton in Skeleton)
+            {
+                if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                {
+                    Joint handJoint = skeleton.Joints[JointType.HandRight];
+                    Joint elbowJoint = skeleton.Joints[JointType.ElbowRight];
+
+                    // Spine
+                    printLine(skeleton.Joints[JointType.Head], skeleton.Joints[JointType.ShoulderCenter]);
+                    printLine(skeleton.Joints[JointType.ShoulderCenter], skeleton.Joints[JointType.Spine]);
+
+                    // left leg
+                    printLine(skeleton.Joints[JointType.Spine], skeleton.Joints[JointType.HipCenter]);
+                    printLine(skeleton.Joints[JointType.HipCenter], skeleton.Joints[JointType.HipLeft]);
+                    printLine(skeleton.Joints[JointType.HipLeft], skeleton.Joints[JointType.KneeLeft]);
+                    printLine(skeleton.Joints[JointType.KneeLeft], skeleton.Joints[JointType.AnkleLeft]);
+                    printLine(skeleton.Joints[JointType.AnkleLeft], skeleton.Joints[JointType.FootLeft]);
+
+                    // rigth leg
+                    printLine(skeleton.Joints[JointType.HipCenter], skeleton.Joints[JointType.HipRight]);
+                    printLine(skeleton.Joints[JointType.HipRight], skeleton.Joints[JointType.KneeRight]);
+                    printLine(skeleton.Joints[JointType.KneeRight], skeleton.Joints[JointType.AnkleRight]);
+                    printLine(skeleton.Joints[JointType.AnkleRight], skeleton.Joints[JointType.FootRight]);
+
+                    // left arm
+                    printLine(skeleton.Joints[JointType.ShoulderCenter], skeleton.Joints[JointType.ShoulderLeft]);
+                    printLine(skeleton.Joints[JointType.ShoulderLeft], skeleton.Joints[JointType.ElbowLeft]);
+                    printLine(skeleton.Joints[JointType.ElbowLeft], skeleton.Joints[JointType.WristLeft]);
+                    printLine(skeleton.Joints[JointType.WristLeft], skeleton.Joints[JointType.HandLeft]);
+
+                    // right arm
+                    printLine(skeleton.Joints[JointType.ShoulderCenter], skeleton.Joints[JointType.ShoulderRight]);
+                    printLine(skeleton.Joints[JointType.ShoulderRight], skeleton.Joints[JointType.ElbowRight]);
+                    printLine(skeleton.Joints[JointType.ElbowRight], skeleton.Joints[JointType.WristRight]);
+                    printLine(skeleton.Joints[JointType.WristRight], skeleton.Joints[JointType.HandRight]);
+
+
+                    foreach (Joint joint in skeleton.Joints)
+                    {
+                        if (skeleton.TrackingState != SkeletonTrackingState.Tracked)
+                            continue;
+
+                        if (joint.JointType == JointType.HandLeft)
+                        {
+                            swipeGestureRecognizer.Add(joint.Position, sensor);
+                        }
+                        else if (joint.JointType == JointType.HandRight)
+                        {
+                            circleGestureRecognizer.Add(joint.Position, sensor);
+                        }
+
+                        Ellipse point = new Ellipse();
+                        point.Stroke = new SolidColorBrush(Colors.Red);
+                        point.StrokeThickness = 8;
+                        DepthImagePoint depthPoint = this.sensor.CoordinateMapper.MapSkeletonPointToDepthPoint(joint.Position, DepthImageFormat.Resolution640x480Fps30);
+                        point.Margin = new Thickness(depthPoint.Y, depthPoint.X, 0, 0);
+                        canvasEsqueleto.Children.Add(point);
+                    }
+                }
+            }
+        }
+
+        private Point SkeletonPointToScreen(SkeletonPoint skelpoint)
+        {
+            // Convert point to depth space.  
+            // We are not using depth directly, but we do want the points in our 640x480 output resolution.
+            DepthImagePoint depthPoint = this.sensor.CoordinateMapper.MapSkeletonPointToDepthPoint(skelpoint, DepthImageFormat.Resolution640x480Fps30);
+            return new Point(depthPoint.X, depthPoint.Y);
+        }
+
+        private void printLine(Joint j1, Joint j2)
+        {
+            Line boneLine = new Line();
+            boneLine.Stroke = new SolidColorBrush(Colors.White);
+            boneLine.StrokeThickness = 5;
+
+            ColorImagePoint j1P = this.sensor.CoordinateMapper.MapSkeletonPointToColorPoint(j1.Position, ColorImageFormat.RgbResolution640x480Fps30);
+            boneLine.X1 = j1P.X;
+            boneLine.Y1 = j1P.Y;
+
+            ColorImagePoint j2P = this.sensor.CoordinateMapper.MapSkeletonPointToColorPoint(j2.Position, ColorImageFormat.RgbResolution640x480Fps30);
+            boneLine.X2 = j2P.X;
+            boneLine.Y2 = j2P.Y;
+
+            canvasEsqueleto.Children.Add(boneLine);
+        }
+
+        private void NoneSelection_Checked(object sender, RoutedEventArgs e)
+        {
+            if (this.sensor != null && this.sensor.IsRunning)
+            {
+                this.sensor.Stop();
+            }
+        }
+
+        private void ColorSelection_Checked(object sender, RoutedEventArgs e)
+        {
+            if (this.sensor != null)
+            {
+                if (!this.sensor.IsRunning) this.sensor.Start();
+                this.sensor.DepthStream.Disable();
+                this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+            }
+        }
+
+        private void IRSelection_Checked(object sender, RoutedEventArgs e)
+        {
+            if (this.sensor != null)
+            {
+                if (!this.sensor.IsRunning) this.sensor.Start();
+                this.sensor.ColorStream.Disable();
+                this.sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+            }
+        }
+
+        private void BoneSelection_Checked(object sender, RoutedEventArgs e)
+        {
+            if (this.sensor != null)
+            {
+                if (!this.sensor.IsRunning) this.sensor.Start();
+                if (this.BoneSelection.IsChecked == true) this.sensor.SkeletonStream.Enable();
+                else this.sensor.SkeletonStream.Disable();
+            }
+        }
+    }
+}
